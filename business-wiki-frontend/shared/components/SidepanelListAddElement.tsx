cb32a9f541b7dbc2f2884bb413b16d525a38ae1e
Strayker <45916090+StraykerPL@users.noreply.github.com>
2022-10-14 12:11:58 +0200

    fix: Repair add page icon on Home page

--- business-wiki-frontend/shared/components/SidepanelListAddElement.tsx
+++ business-wiki-frontend/shared/components/SidepanelListAddElement.tsx
@@ -1,12 +1,12 @@
 import listElementStyles from "../styles/SidepanelListElement.module.scss";
+import { MdAdd } from "react-icons/md";
 
 import { MouseEventHandler } from "react";
 
 const SidepanelListAddElement = (props: { addPageEvent: MouseEventHandler<HTMLDivElement> }) => {
-    const icon = () => <img src="../assets/add-button.svg" alt="Add New Page" />;
   return (
     <div className={listElementStyles.container} onClick={props.addPageEvent} >
-        {icon()}
+        <MdAdd className={listElementStyles.container__icon} />
     </div>
   );
 };
--- business-wiki-frontend/shared/models/Page.tsx
+++ business-wiki-frontend/shared/models/Page.tsx
@@ -1,6 +1,7 @@
 import { UserInterface } from "./UserInterface"
 
 export interface Page {
+    id: number,
     title: string,
     content: string
     createdBy: UserInterface
